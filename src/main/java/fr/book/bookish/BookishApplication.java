d666beee1be6f8d374faa08fce7d5738f3709154
sanjana1202 <sanjana.raparthii@gmail.com>
2023-06-17 23:49:38 +0200

    Refactor:Adds @Component for package scanning

--- src/main/java/fr/book/bookish/BookishApplication.java
+++ src/main/java/fr/book/bookish/BookishApplication.java
@@ -2,8 +2,10 @@ package fr.book.bookish;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"controller"})
 public class BookishApplication {
 
 	public static void main(String[] args) {
