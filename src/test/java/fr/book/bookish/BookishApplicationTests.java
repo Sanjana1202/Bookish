5a173f42a93502fa5b06661b2f9d1cdc08f81bd6
sanjana1202 <sanjana.raparthii@gmail.com>
2023-06-17 12:03:02 +0200

    Inital Commit

---
+++ src/test/java/fr/book/bookish/BookishApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.book.bookish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
