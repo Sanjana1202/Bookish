3bf782e4002e3fab10f58e3acdd456d71ab64d7e
sanjana1202 <sanjana.raparthii@gmail.com>
2023-06-18 15:19:05 +0200

    Feat:Adds Bookrepository Interface To implemnt CRUD operations

---
+++ src/main/java/Repository/BookRepository.java
@@ -0,0 +1,14 @@
+package Repository;
+
+
+import datamodel.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+//defining Book repository to manage CRUD operations
+//@Repository is used to declare a class as DAO
+//the class extends to JpaRepository which implements all CRUD operations
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
+}
